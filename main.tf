42bd3ccf4e1ba4e57aa189669087dbd5e7271fe8
kmiszel <k.miszel@protonmail.com>
2023-11-06 15:52:40 +0100

    checkov5

--- main.tf
+++ main.tf
@@ -34,6 +34,10 @@ resource "azurerm_resource_group" "testRG" {
 
 resource "azurerm_storage_account" "testSA" {
   name                     = "kmiszeltestsa12"
+  account_kind = "StorageV2"
+  identity {
+    type = "UserAssigned"
+  }
   resource_group_name      = azurerm_resource_group.testRG.name
   location                 = azurerm_resource_group.testRG.location
   account_tier             = "Standard"
@@ -42,6 +46,10 @@ resource "azurerm_storage_account" "testSA" {
   enable_https_traffic_only = true
   public_network_access_enabled = false
   allow_nested_items_to_be_public = false
+  customer_managed_key {
+    key_vault_key_id = azurerm_key_vault_key.example.id
+    user_assigned_identity_id = ""
+  }
   queue_properties  {
   logging {
         delete                = true
@@ -52,3 +60,28 @@ resource "azurerm_storage_account" "testSA" {
     }
   }
 }
+
+resource "azurerm_key_vault" "example" {
+  name                = "example-keyvault"
+  resource_group_name = azurerm_resource_group.testRG.name
+  location            = azurerm_resource_group.testRG.location
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = ["get", "create", "wrapKey", "unwrapKey", "sign", "verify"]
+  }
+}
+
+resource "azurerm_key_vault_key" "example" {
+  name         = "example-cmk"
+  key_vault_id = azurerm_key_vault.example.id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts     = ["decrypt", "encrypt", "sign", "verify"]
+}
+
+data "azurerm_client_config" "current" {}
