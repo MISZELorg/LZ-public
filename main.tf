2233e624a1fcd07160909f4ca42a308ede0196d9
kmiszel <k.miszel@protonmail.com>
2023-11-20 14:01:24 +0100

    correct5

--- main.tf
+++ main.tf
@@ -57,8 +57,8 @@ resource "azurerm_linux_web_app" "example" {
   resource_group_name        = azurerm_resource_group.testRG.name
   location                   = azurerm_resource_group.testRG.location
   service_plan_id            = azurerm_service_plan.example-asp.id
-  client_certificate_enabled = true
-  client_certificate_mode    = "Required"
+  client_certificate_enabled = false
+  # client_certificate_mode    = "Required"
   auth_settings {
     enabled = true
   }
