90e7c1976eabc5b23ffb88000354c89fc223820b
kmiszel <k.miszel@protonmail.com>
2023-11-20 12:42:35 +0100

    https

--- main.tf
+++ main.tf
@@ -31,6 +31,6 @@ resource "azurerm_storage_account" "example" {
   resource_group_name       = azurerm_resource_group.testRG.name
   location                  = azurerm_resource_group.testRG.location
   account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  enable_https_traffic_only = false
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = true
 }
