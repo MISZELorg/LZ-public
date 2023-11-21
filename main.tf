788af60f22a7fca98c311a637cdd46290a198646
kmiszel <k.miszel@protonmail.com>
2023-11-21 14:17:37 +0100

    check

--- main.tf
+++ main.tf
@@ -31,18 +31,18 @@ resource "azurerm_resource_group" "testRG" {
 #   location = var.location
 # }
 
-# resource "azurerm_storage_account" "example-sa" {
-#   name                     = "kmiszeltfsec"
-#   resource_group_name      = azurerm_resource_group.testRG.name
-#   location                 = azurerm_resource_group.testRG.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   # enable_https_traffic_only = true
-#   # min_tls_version = "TLS1_2"
-#   # identity {
-#   #   type = "SystemAssigned"
-#   # }
-# }
+resource "azurerm_storage_account" "example-sa" {
+  name                     = "kmiszeltfsec"
+  resource_group_name      = azurerm_resource_group.testRG.name
+  location                 = azurerm_resource_group.testRG.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  # enable_https_traffic_only = true
+  # min_tls_version = "TLS1_2"
+  # identity {
+  #   type = "SystemAssigned"
+  # }
+}
 
 # resource "azurerm_storage_account" "example" {
 #   name                      = "kmiszelsonarcloud1"
